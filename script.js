1783fd085ef3f77158ff58bd4b2b5b1782b8bfd4
qimeraadva <126074255+qimeraadva@users.noreply.github.com>
2023-10-16 16:17:13 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,43 @@
+//1) forEach
+// let numbers = [5,6,7,8,9,10]
+// let result = numbers.forEach((nums) => {
+//     console.log(nums*2);
+// })
+//2) .map
+// let numbers = [2,4,5,3,6,7,9];
+// let doubleNums = numbers.map((nums) => nums*2);
+// console.log(doubleNums)
+//3) filter
+// let numbers = [5,3,2,1,6,7,9,32,67,50,100];
+// let filteredNums = numbers.filter((nums) => nums%2 === 0);
+// console.log(filteredNums);
+// break 3 element array into 3 variables
+// let sports = ['football, basketball, golf, swim'];
+// let [sport1 ,sport2, sport3, sport4] = sports;
+// console.log(sport1, sport2, sport3, sport4)
+// in object give  each property new names
+// let object = {
+//     property1: "value1",
+//     property2: "value2",
+//     property3: "value3"
+//   };
+  
+//   let { property1, property2, property3 } = object;
+  
+//   let newNames = {
+//     newName1: property1,
+//     newName2: property2,
+//     newName3: property3
+//   };
+  
+//   console.log(newNames);
+
+//reduce
+
+// let numbers = [1, 2, 3, 4, 5];
+// let sum = numbers.reduce((accumulator, nums) => {
+//   return accumulator + nums;
+// },  null);
+// console.log(sum);
+
+  
